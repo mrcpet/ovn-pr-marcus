bd2dafac2b8539a4f7bf5fb895771524ddad111d
unoriginalnickname <aadammatthews@gmail.com>
2026-05-25 11:53:17 +0200

    bra kod

--- ExceptionsDemo/Program.cs
+++ ExceptionsDemo/Program.cs
@@ -58,6 +58,10 @@
                 {
                     throw new ArgumentException("Filnamn får inte vara tomt eller null.", nameof(fileName));
                 }
+                for (int i = 0; i < 5; i++)
+                {
+                    i = i % 4; //vroom
+                }
 
                 StreamReader? reader = null;
                 try
